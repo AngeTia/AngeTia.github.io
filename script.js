cb8d1818c2b38292f1de61c1ecaf7f28520a6d13
M. Gompou Tia Ange <tiaange03@gmail.com>
2024-06-21 10:44:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ canvas.height = window.innerHeight;
 
 const letters = Array(256).join(1).split('');
 let intervalId = null;
+let timeoutId = null;
+const animationDuration = 60000; // 1 minute in milliseconds
+const intervalBetweenAnimations = 10000; // 10 seconds in milliseconds
 
 const draw = () => {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
@@ -23,14 +26,22 @@ const startAnimation = () => {
   if (!intervalId) {
     intervalId = setInterval(draw, 33);
   }
+  // Stop animation after the specified duration
+  timeoutId = setTimeout(stopAnimation, animationDuration);
 };
 
 const stopAnimation = () => {
   if (intervalId) {
     clearInterval(intervalId);
     intervalId = null;
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas when stopping the animation
   }
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+    timeoutId = null;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas when stopping the animation
+  // Restart the animation after a pause
+  setTimeout(startAnimation, intervalBetweenAnimations);
 };
 
 canvas.addEventListener('mouseover', stopAnimation);
@@ -41,4 +52,5 @@ window.addEventListener('resize', () => {
   canvas.height = window.innerHeight;
 });
 
-startAnimation(); // Start the animation initially
+// Start the animation initially
+startAnimation();
