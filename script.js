7eab4cda820925a6214813f8117a1bfa31f976a1
M. Gompou Tia Ange <tiaange03@gmail.com>
2024-06-21 10:36:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const letters = Array(256).join(1).split('');
+let intervalId = null;
 
 const draw = () => {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
@@ -18,9 +19,26 @@ const draw = () => {
   });
 };
 
-setInterval(draw, 33);
+const startAnimation = () => {
+  if (!intervalId) {
+    intervalId = setInterval(draw, 33);
+  }
+};
+
+const stopAnimation = () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas when stopping the animation
+  }
+};
+
+canvas.addEventListener('mouseover', stopAnimation);
+canvas.addEventListener('mouseout', startAnimation);
 
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
+
+startAnimation(); // Start the animation initially
